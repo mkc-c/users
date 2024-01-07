6dc80c748b6aaba9d5b8e6806eff2e7fbeef828b
mkc-c <abdirisak.arr@gmail.com>
2024-01-07 18:42:32 +0000

    create and pass down onUsersAdd handler, add UserList componenet, create user state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 import UserForm from "./UserForm";
+import UserList from "./UserList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUsersAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
   return (
     <div>
-      <UserForm />
+      <UserForm onUsersAdd={onUsersAdd} />
+      <hr />
+      <UserList users={users} />
     </div>
   );
 }
