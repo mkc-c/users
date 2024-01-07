d4726627928d53f95b1a7f8876f1439b31471daa
mkc-c <abdirisak.arr@gmail.com>
2024-01-07 12:45:39 +0000

    create UserList component, make input fields controlled in UseForm component

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 function UserForm() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
   return (
     <form>
       <div>
         <label>Name</label>
-        <input />
+        <input value={name} onChange={(event) => setName(event.target.value)} />
       </div>
       <div>
         <label>Email</label>
-        <input />
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </div>
       <button>Add User</button>
     </form>
