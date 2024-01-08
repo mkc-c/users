a039b600d589684157a2e82b4f24ec62dde580d0
mkc-c <abdirisak.arr@gmail.com>
2024-01-08 17:01:23 +0000

    add render one row user test logic and assertion

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,6 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {});
+test("render one row per user", () => {
+  const users = [
+    {
+      name: "jane",
+      email: "jane@jane.com",
+    },
+    {
+      name: "sam",
+      email: "sam@sam.com",
+    },
+  ];
+
+  render(<UserList users={users} />);
+
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
+
+  expect(rows).toHaveLength(2);
+});
 
 test("render the email and name of each user", () => {});
