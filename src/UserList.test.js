897e992150dd35419cd5427f44570171642234a4
mkc-c <abdirisak.arr@gmail.com>
2024-01-08 07:00:13 +0000

    create test stubs

---
+++ src/UserList.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import UserList from "./UserList";
+
+test("render one row per user", () => {});
+
+test("render the email and name of each user", () => {});
