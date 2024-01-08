d36e3ce764c1c24ef2d3de7bc6a1ed1cfd21b2ac
mkc-c <abdirisak.arr@gmail.com>
2024-01-08 05:47:15 +0000

    change element matchers to more generic matchers

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -17,7 +17,8 @@ test("it calls onUserAdd when the form is submitted", () => {
 
   render(<UserForm onUserAdd={mock} />);
 
-  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
 
   user.click(nameInput);
   user.keyboard("jane");
