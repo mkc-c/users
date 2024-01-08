526aac9be4247df6594916981d0b52ad04e9b4fc
mkc-c <abdirisak.arr@gmail.com>
2024-01-08 05:18:43 +0000

    add unideal 'it calls on user add when form is submitted' test case

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -2,9 +2,36 @@ import { render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import UserForm from "./UserForm";
 
-test("it shows two inputs and a button", async () => {
+test("it shows two inputs and a button", () => {
   render(<UserForm />);
 
   const inputs = screen.getAllByRole("textbox");
   const button = screen.getByRole("button");
+
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test("it calls onUserAdd when the form is submitted", () => {
+  const argList = [];
+  const callBack = (...args) => {
+    argList.push(args);
+  };
+
+  render(<UserForm onUserAdd={callBack} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  const button = screen.getByRole("button");
+
+  user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@jane.com" });
 });
