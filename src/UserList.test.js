4e67f2909c0964f3f83e66c38a67830ee351de19
mkc-c <abdirisak.arr@gmail.com>
2024-01-09 02:07:51 +0000

    refactor to DRY implementation

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,7 +1,7 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
+function renderComponent() {
   const users = [
     {
       name: "jane",
@@ -14,6 +14,12 @@ test("render one row per user", () => {
   ];
 
   render(<UserList users={users} />);
+
+  return { users };
+}
+
+test("render one row per user", () => {
+  renderComponent();
 
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
@@ -21,18 +27,7 @@ test("render one row per user", () => {
 });
 
 test("render the email and name of each user", () => {
-  const users = [
-    {
-      name: "jane",
-      email: "jane@jane.com",
-    },
-    {
-      name: "sam",
-      email: "sam@sam.com",
-    },
-  ];
-
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole("cell", { name: user.name });
