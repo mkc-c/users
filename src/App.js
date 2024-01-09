523bcac778dcf58904e7579219c5243c76dfeb8d
mkc-c <abdirisak.arr@gmail.com>
2024-01-09 15:43:51 +0000

    meant open closed branch not stale, fied typo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div>
-      <UserForm onUsersAdd={onUserAdd} />
+      <UserForm onUserAdd={onUserAdd} />
       <hr />
       <UserList users={users} />
     </div>
